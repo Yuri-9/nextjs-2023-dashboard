c337d71c7e840734282bc1568c3365b3824a0ea9
Yuri-9 <vashkevichyp@gmail.com>
2023-12-07 15:36:04 +0300

    feat: 11-14 chapters

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -10,6 +10,7 @@ import {
 } from './definitions';
 import { formatCurrency } from './utils';
 import { unstable_noStore as noStore } from 'next/cache';
+import { notFound } from 'next/navigation';
 
 export async function fetchRevenue() {
   // Add noStore() here prevent the response from being cached.
